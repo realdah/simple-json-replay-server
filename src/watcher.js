66ddebc64a9cdc442b3e4ac3aa456e248cee9c77
Jeff <realdah@gmail.com>
2017-03-17 00:08:08 -0400

    Introduce rxjs debounce for file watching.

--- src/watcher.js
+++ src/watcher.js
@@ -1,4 +1,5 @@
 var chokidar = require('chokidar');
+var Rx = require( 'rx' );
 var mockDataLoader = require("./mockDataLoader");
 var util = require("./util");
 
@@ -6,16 +7,25 @@ var log = console.log;
 
 function startWatching(folder) {
 
-    var watcher = chokidar.watch(folder, {
+    var dirWatcher = chokidar.watch(folder, {
         ignored: /[\/\\]\./, persistent: true
     });
 
-    watcher.on('change', function (path) {
+    //Only reload the files until user stops changing files after 800 milliseconds, to avoid repeat loading when saving/copying multiple files.
+    var changedFiles = Rx.Observable.fromEvent( dirWatcher, 'change' ).debounce(function (x) { return Rx.Observable.timer(800); });
+
+    var subscription = changedFiles.subscribe(
+    function( filename ) {
             util.print("Change detected for mock data, reload...");
             mockDataLoader.reset();
             mockDataLoader.loadRequestMappings(folder);
-        })
-
+    },
+    function( err ) {
+        console.log( 'Error: ' + err );
+    },
+    function () {
+        console.log( 'Completed' );
+    });
 }
 exports.startWatching = startWatching;
 
